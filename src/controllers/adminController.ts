cdfc9c235dfb077729b870dd36d8974897fe0237
Jaimin Raval <jaiminraval100@gmail.com>
2025-07-10 22:28:47 +0530

    commit

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import User from '../models/User';
-import { verifyToken, isAdmin } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
